021d2d0f9350b9998723cacff366bc4fc794cac7
Brian Fajardo <bmf5@hotmail.com>
2017-06-08 20:24:10 -0400

    Initial commit

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <h1>
+    It&apos;s alive muahahaha
+     <span role="img" aria-label="lightning">⚡️️</span>
+  </h1>,
+  document.getElementById('root')
+)
