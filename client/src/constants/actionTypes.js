02ca5041f9a59cd1fb96c82e5f1d7d22fde72711
Brian Fajardo <bmf5@hotmail.com>
2017-06-30 01:12:16 -0400

    Fix client HTTPS request URL in production

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,5 +1,5 @@
 // Production Express server
-export const URL = 'https://mernstack-todos.herokuapp.com/'
+export const URL = 'https://mernstack-todos.herokuapp.com'
 
 // User input
 export const INPUT_VALUE = 'INPUT_VALUE'
